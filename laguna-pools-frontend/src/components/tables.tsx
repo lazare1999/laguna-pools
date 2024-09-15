83ce35ec4df029945b4573a2a6baf9bb57b35e02
akakh18 <akakh18@freeuni.edu.ge>
2024-09-15 16:38:13 +0400

    Add client adding logic

--- laguna-pools-frontend/src/components/tables.tsx
+++ laguna-pools-frontend/src/components/tables.tsx
@@ -1,5 +1,7 @@
 import React, {useState} from "react";
 import {
+    Box,
+    Button,
     IconButton,
     Paper,
     Table,
@@ -11,15 +13,18 @@ import {
     TableRow,
     TextField,
 } from "@mui/material";
+import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
 import {FilterList} from "@mui/icons-material";
 import UserRow from "./userRow";
 import {initialUsers, User} from "../utils/mockUsers";
+import AddUserDialog from "./addUserDialog";
 
 const ClientsTable: React.FC = () => {
     const [filterText, setFilterText] = useState<string>("");
     const [users, setUsers] = useState<User[]>(initialUsers);
     const [page, setPage] = useState<number>(0);
     const [rowsPerPage, setRowsPerPage] = useState<number>(5);
+    const [openDialog, setOpenDialog] = useState<boolean>(false);
 
     const handleDelete = (userToDelete: User) => {
         setUsers(users.filter(user => user !== userToDelete));
@@ -44,23 +49,52 @@ const ClientsTable: React.FC = () => {
         setPage(0);
     };
 
+    const handleOpenDialog = () => {
+        setOpenDialog(true);
+    };
+
+    const handleCloseDialog = () => {
+        setOpenDialog(false);
+    };
+
+    const handleAddUser = (newUser: User) => {
+        setUsers([...users, newUser]);
+    };
+
     return (
         <Paper>
-            <TextField
-                label="Filter by name"
-                variant="outlined"
-                value={filterText}
-                onChange={handleFilterChange}
-                fullWidth
-                margin="normal"
-                InputProps={{
-                    endAdornment: (
-                        <IconButton>
-                            <FilterList/>
-                        </IconButton>
-                    ),
-                }}
-            />
+            <Box sx={{display: "flex", alignItems: "center", justifyContent: "space-between", padding: 2}}>
+                <TextField
+                    label="Filter by name"
+                    variant="outlined"
+                    value={filterText}
+                    onChange={handleFilterChange}
+                    fullWidth
+                    margin="normal"
+                    InputProps={{
+                        endAdornment: (
+                            <IconButton>
+                                <FilterList/>
+                            </IconButton>
+                        ),
+                    }}
+                    sx={{flexGrow: 1}}
+                />
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={handleOpenDialog}
+                    startIcon={<PersonAddAltIcon/>}
+                    sx={{
+                        ml: 2,
+                        height: "50px",
+                        display: "flex",
+                        alignItems: "center",
+                    }}
+                >
+                    New User
+                </Button>
+            </Box>
             <TableContainer>
                 <Table>
                     <TableHead>
@@ -90,6 +124,7 @@ const ClientsTable: React.FC = () => {
                 onPageChange={handlePageChange}
                 onRowsPerPageChange={handleRowsPerPageChange}
             />
+            <AddUserDialog open={openDialog} onClose={handleCloseDialog} onAddUser={handleAddUser}/>
         </Paper>
     );
 };
