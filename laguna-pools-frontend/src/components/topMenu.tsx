bcffb90007d9f0179e03603c421a2fc2d0ce6bc5
lazare1999 <lazarekvirtia@gmail.com>
2024-09-15 21:46:19 +0400

    Replace deprecated @mui/material/Grid -> @mui/material/Grid2

--- laguna-pools-frontend/src/components/topMenu.tsx
+++ laguna-pools-frontend/src/components/topMenu.tsx
@@ -1,8 +1,9 @@
 import React, {useEffect, useState} from 'react';
-import {AppBar, Box, Button, Grid, Toolbar, Typography} from '@mui/material';
+import {AppBar, Box, Button, Toolbar, Typography} from '@mui/material';
 import {LOCAL_STORAGE_NAME} from "../utils/constants";
 import {Component} from "../utils/componentsEnum";
 import componentNameMapper from "../utils/componentNameMapper";
+import Grid from '@mui/material/Grid2';
 
 interface TopMenuProps {
     select: number;
@@ -21,7 +22,7 @@ const TopMenu: React.FC<TopMenuProps> = ({select, selectHandler}) => {
         <AppBar position="static">
             <Toolbar>
                 <Grid container alignItems="center">
-                    <Grid item xs={2}>
+                    <Grid size={2}>
                         <Box>
                             {token !== "test_token" &&
                                 <Button color="inherit" onClick={() => selectHandler(Component.LOGIN)}>Login</Button>}
@@ -33,13 +34,13 @@ const TopMenu: React.FC<TopMenuProps> = ({select, selectHandler}) => {
                         </Box>
                     </Grid>
 
-                    <Grid item xs={8} container justifyContent="center">
+                    <Grid size={8} container justifyContent="center">
                         <Typography variant="h6" component="div">
                             {componentNameMapper(select)}
                         </Typography>
                     </Grid>
 
-                    <Grid item xs={2}/>
+                    <Grid size={2}/>
                 </Grid>
             </Toolbar>
         </AppBar>
