bc45b13a9fb589ab4711c945a6169d800876d1a6
lazare1999 <lazarekvirtia@gmail.com>
2024-09-16 21:58:25 +0400

    Add "V3__insert_new_role_change_password.sql"

---
+++ src/main/resources/db/migration/V3__insert_new_role_change_password.sql
@@ -0,0 +1,2 @@
+INSERT INTO users.targets (target_id, target_name, target_description, app_id, order_by, group_by)
+VALUES (3, 'ROLE_LAGUNA_CHANGE_PASSWORD', 'laguna user who can change password', 1, 3, 'laguna')
