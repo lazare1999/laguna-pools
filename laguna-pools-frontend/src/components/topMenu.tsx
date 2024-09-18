6dbc0c930006c0b2831c7222c2febc5d7c8e0a3d
akakh18 <akakh18@freeuni.edu.ge>
2024-09-19 00:20:01 +0400

    Revert transfer to signin after logout

--- laguna-pools-frontend/src/components/topMenu.tsx
+++ laguna-pools-frontend/src/components/topMenu.tsx
@@ -23,15 +23,11 @@ const TopMenu: React.FC<TopMenuProps> = ({selectHandler}) => {
     };
 
     const logOutHandler = () => {
-        console.log("HERE!");
         localStorage.removeItem(REFRESH_TOKEN_NAME);
         localStorage.removeItem(REFRESH_TOKEN_EXP_NAME);
         console.log(`${REFRESH_TOKEN_NAME}: ${localStorage.getItem(REFRESH_TOKEN_NAME)}`);
         console.log(`${REFRESH_TOKEN_NAME} and ${REFRESH_TOKEN_EXP_NAME} removed!`);
-
-        setTimeout(() => {
-            // selectHandler(Component.LOGIN);
-        }, 5000);
+        selectHandler(Component.LOGIN);
     }
 
     return (
