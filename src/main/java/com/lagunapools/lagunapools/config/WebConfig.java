dedcc20ff134647b1fa95d86582dbc247c9ab714
lazare1999 <lazarekvirtia@gmail.com>
2024-09-19 11:50:29 +0400

    Exclude WebConfig.java from Git

---
+++ src/main/java/com/lagunapools/lagunapools/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.lagunapools.lagunapools.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by Lazo on 9/19/24
+ */
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
