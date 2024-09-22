94a8ad42076bff69c172814451244451cbf751f6
akakh18 <akakh18@freeuni.edu.ge>
2024-09-23 00:09:29 +0400

    Return actual clients

--- src/main/java/com/lagunapools/lagunapools/app/clients/service/ClientsServiceImpl.java
+++ src/main/java/com/lagunapools/lagunapools/app/clients/service/ClientsServiceImpl.java
@@ -5,8 +5,14 @@ import com.lagunapools.lagunapools.app.clients.models.AllClientsResponseDTO;
 import com.lagunapools.lagunapools.app.clients.models.ClientDTO;
 import com.lagunapools.lagunapools.app.clients.repository.ClientEntity;
 import com.lagunapools.lagunapools.app.clients.repository.ClientsRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
 
 import static com.lagunapools.lagunapools.utils.ResponseUtils.badRequestResponse;
 import static com.lagunapools.lagunapools.utils.ResponseUtils.okResponse;
@@ -32,13 +38,18 @@ public class ClientsServiceImpl implements ClientsService {
 
     @Override
     public AllClientsResponseDTO getAllClients(AllClientsRequestDTO request) {
-//        if (Objects.isNull(request)
-//                || request.getPageKey() == null
-//                || request.getPageSize() == null) {
-//            return new AllClientsResponseDTO();
-//        }
-//
-//        return clientsRepository.findAll();
-        return null;
+        if (Objects.isNull(request)
+                || request.getPageKey() == null
+                || request.getPageSize() == null) {
+            return new AllClientsResponseDTO();
+        }
+
+        Pageable pageable = PageRequest.of(request.getPageKey(), request.getPageSize());
+
+
+        Page<ClientEntity> clientPage = clientsRepository.findAll(pageable);
+        List<ClientDTO> clientsList = clientPage.stream().map(ClientDTO::new).toList();
+
+        return new AllClientsResponseDTO(clientPage.getTotalElements(), clientsList);
     }
 }
