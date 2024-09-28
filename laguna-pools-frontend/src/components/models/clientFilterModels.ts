321cfadbe29f852c2cb4b3cdcef52dd4035aff65
lazare1999 <lazarekvirtia@gmail.com>
2024-09-28 16:58:35 +0400

    Add filters to clients

---
+++ laguna-pools-frontend/src/components/models/clientFilterModels.ts
@@ -0,0 +1,43 @@
+export interface ClientFilters {
+    name: string;
+    lastName: string;
+}
+
+export const defaultClientFilters: ClientFilters = {
+    name: '',
+    lastName: ''
+};
+
+export interface DialogFilters {
+    contractStatus: boolean;
+    idStatus: boolean;
+    phone: string;
+    parent: string;
+    birthDayFrom: string;
+    birthDayTo: string;
+    expDayFrom: string;
+    expDayTo: string;
+    docDayFrom: string;
+    docDayTo: string;
+    costFrom: number;
+    costTo: number;
+    selectedGroups: string[];
+    notes: string;
+}
+
+export const defaultDialogFilters: DialogFilters = {
+    contractStatus: false,
+    idStatus: false,
+    phone: '',
+    parent: '',
+    birthDayFrom: '',
+    birthDayTo: '',
+    expDayFrom: '',
+    expDayTo: '',
+    docDayFrom: '',
+    docDayTo: '',
+    costFrom: 0.0,
+    costTo: 0.0,
+    selectedGroups: [],
+    notes: ''
+};
--- src/main/java/com/lagunapools/lagunapools/app/clients/service/ClientsService.java
+++ src/main/java/com/lagunapools/lagunapools/app/clients/service/ClientsService.java
@@ -3,7 +3,10 @@ package com.lagunapools.lagunapools.app.clients.service;
 import com.lagunapools.lagunapools.app.clients.models.AllClientsRequestDTO;
 import com.lagunapools.lagunapools.app.clients.models.AllClientsResponseDTO;
 import com.lagunapools.lagunapools.app.clients.models.ClientDTO;
+import com.lagunapools.lagunapools.app.clients.models.GroupDTO;
 import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 public interface ClientsService {
 
@@ -13,4 +16,5 @@ public interface ClientsService {
 
     ResponseEntity<?> getClient(Long clientId);
 
+    List<GroupDTO> listGroups();
 }
