ae2b5a7cb1ae6c0b5fa0f794ab31e8b5d41e8bc5
lazare1999 <lazarekvirtia@gmail.com>
2024-10-01 21:31:27 +0400

    Change deleteClient method

--- src/main/java/com/lagunapools/lagunapools/app/clients/repository/ClientGroupsRepository.java
+++ src/main/java/com/lagunapools/lagunapools/app/clients/repository/ClientGroupsRepository.java
@@ -1,13 +1,7 @@
 package com.lagunapools.lagunapools.app.clients.repository;
 
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 
 public interface ClientGroupsRepository extends JpaRepository<ClientGroupsEntity, Long> {
-
-    @Transactional
-    @Modifying(clearAutomatically = true)
-    void deleteAllByClientId(Long clientId);
 
 }
