6f34de9ff4ea0bf15a01440edc1ded4223463033
lazare1999 <lazarekvirtia@gmail.com>
2024-10-17 13:31:23 +0400

    Add toast to excel

--- laguna-pools-frontend/src/components/clients/excelImport.tsx
+++ laguna-pools-frontend/src/components/clients/excelImport.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {fetchExcelFile} from "../../utils/excel";
 import {Button} from '@mui/material';
 import UploadFileIcon from '@mui/icons-material/UploadFile';
 import {TABLE_BUTTON_STYLES} from "../../utils/constants";
 import ApiService from "../../api/api";
 import {HttpMethod} from "../../utils/enums/httpMethodEnum";
+import {Toast} from "../../utils/alertsUtils";
 
 const ExcelImport: React.FC = () => {
+
+    const [toastOpen, setToastOpen] = useState<boolean>(false);
+    const [toastMessage, setToastMessage] = useState<string>("");
+
+
     const handleFileUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files?.[0];
 
@@ -15,7 +21,12 @@ const ExcelImport: React.FC = () => {
                 const parsedClients = await fetchExcelFile(file);
                 ApiService.request("clients/list", HttpMethod.POST,
                     {clients: parsedClients}
-                ).catch(err => console.error(err));
+                ).then(res => {
+                    if (res.status === 200) {
+                        setToastMessage(res.data);
+                        setToastOpen(true);
+                    }
+                }).catch(err => console.error(err));
             } catch (error) {
                 console.error('Error reading Excel file:', error);
             }
@@ -24,6 +35,12 @@ const ExcelImport: React.FC = () => {
 
     return (
         <div>
+            <Toast
+                open={toastOpen}
+                message={toastMessage}
+                onClose={() => setToastOpen(false)}
+                options={{autoHideDuration: 3000}}
+            />
             <input
                 type="file"
                 accept=".xlsx, .xls"
